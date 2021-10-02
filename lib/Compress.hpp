f28bc8d2aa1ffe3e1823c91979d9668d69627713
anton <hashlib@yandex.ru>
2021-10-02 06:45:24 +0300

    Move structures to InternalStructures.hpp

--- lib/Compress.hpp
+++ lib/Compress.hpp
@@ -1,56 +1,26 @@
 #pragma once
 
-#include <array>
-#include <unordered_map>
-#include <vector>
-
 #include "BinaryTree.hpp"
 #include "InputBitStream.hpp"
+#include "InternalStructures.hpp"
 #include "OutputBitStream.hpp"
 
 namespace Archiver {
-enum class ControlCharacters {
-    FILENAME_END = 0b00000001,    // 256 in little-endian bits.
-    ONE_MORE_FILE = 0b100000001,  // 257 in little-endian bits.
-    ARCHIVE_END = 0b010000001,    // 258 in little-endian bits.
-};
 
 void Compress(const std::string& archive_name, const std::vector<std::string>& files_to_archive);
 void CompressFile(const std::string& file_name, InputBitStream& in, OutputBitStream& out, bool is_last);
 
-using FrequencyList = std::array<size_t, 1 << 9>;
 FrequencyList GetFrequencyList(const std::string& filename,
                                InputBitStream& ibitstream,
                                const std::vector<ControlCharacters>& additional_characters);
 
 BinaryTree* GetBinaryTree(const FrequencyList& frequency_list);
 
-struct CodeWord {
-    unsigned short character = 0;
-    std::vector<bool> code;
-
-    bool operator==(const CodeWord& other) const {
-        return character == other.character && code == other.code;
-    }
-};
-
-using Codebook = std::vector<CodeWord>;
 Codebook GetCodebook(const BinaryTree* tree_root);
 
-struct CanonicalCodebook {
-    std::vector<size_t> word_count_by_bit_count;  // shifted by one index to the left.
-    std::vector<unsigned short> characters;
-
-    bool operator==(const CanonicalCodebook& other) const {
-        return word_count_by_bit_count == other.word_count_by_bit_count
-            && characters == other.characters;
-    }
-};
 CanonicalCodebook GetCanonicalCodebook(Codebook regular_codebook);
 
-using EncodingTable = std::unordered_map<unsigned short, std::vector<bool>>;
 EncodingTable GetEncodingTable(Codebook codebook);
-
 void Encode(InputBitStream& in, const EncodingTable& encoding_table,
             OutputBitStream& out, ControlCharacters last_character);
 }  // namespace Archiver
---
+++ lib/InternalStructures.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <array>
+#include <vector>
+#include <unordered_map>
+
+namespace Archiver {
+enum class ControlCharacters {
+    FILENAME_END = 0b00000001,    // 256 in little-endian bits.
+    ONE_MORE_FILE = 0b100000001,  // 257 in little-endian bits.
+    ARCHIVE_END = 0b010000001,    // 258 in little-endian bits.
+};
+
+using FrequencyList = std::array<size_t, 1 << 9>;
+
+struct CodeWord {
+    unsigned short character = 0;
+    std::vector<bool> code;
+
+    bool operator==(const CodeWord& other) const {
+        return character == other.character && code == other.code;
+    }
+};
+
+using Codebook = std::vector<CodeWord>;
+
+struct CanonicalCodebook {
+    std::vector<size_t> word_count_by_bit_count;  // shifted by one index to the left.
+    std::vector<unsigned short> characters;
+
+    bool operator==(const CanonicalCodebook& other) const {
+        return word_count_by_bit_count == other.word_count_by_bit_count
+            && characters == other.characters;
+    }
+};
+
+using EncodingTable = std::unordered_map<unsigned short, std::vector<bool>>;
+}  // namespace Archiver
